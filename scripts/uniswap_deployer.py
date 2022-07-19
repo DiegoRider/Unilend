d7e2d0e15cd5d68ded7ffc3ca0e8bd0ee488d7f0
maxime delespaul <mx.delespaul@gmail.com>
2022-07-19 02:07:56 +0200

    Correctif scripts/uniswap_deployer

--- scripts/uniswap_deployer.py
+++ scripts/uniswap_deployer.py
@@ -1,8 +1,14 @@
+from logging import exception
 from brownie import *
-from brownie import TestToken, SupplyContract, project, accounts
-from web3 import EthereumTesterProvider
+from brownie import TestToken, SupplyContract, project, accounts, UniUtils
+from eth_typing import BlockNumber
+#from web3 import *
+#import web3.eth as wweb3
 from pathlib import Path
 import math
+
+
+
 
 def main():
     # on déploie 2 tokens (pour pouvoir créer une position uniswap v3)
@@ -20,8 +26,7 @@ def main():
     # -------------------------------------------------------
     #address _factory
     uniswap_core = project.load("Uniswap/v3-core@1.0.0")
-    UniFactory = uniswap_core.UniswapV3Factory
-    uni_factory = UniFactory.deploy({"from": accounts[0]})
+    uni_factory = uniswap_core.UniswapV3Factory.deploy({"from": accounts[0]})
     print(uni_factory)
 
     #address _WETH9
@@ -57,10 +62,57 @@ def main():
     x96_price = sqrtRatioX96_ofPrice(price)
 
     # NonFungiblePositionManager
-    nfPosManager = project.load("Uniswap/v3-periphery@1.0.0").NonfungiblePositionManager
-    myPosManager = nfPosManager.deploy(uni_factory, weth9, tokenDescriptor, {"from": accounts[0]})
-    print(myPosManager)
+    nft_pos_manager = project.load("Uniswap/v3-periphery@1.0.0").NonfungiblePositionManager
+    pos_manager = nft_pos_manager.deploy(uni_factory, weth9, tokenDescriptor, {"from": accounts[0]})
+    print(pos_manager)
 
     # initialize pool
-    myPosManager.createAndInitializePoolIfNecessary(token_a, token_b, fee, x96_price, {"from": accounts[0]})
+    my_pool_add = pos_manager.createAndInitializePoolIfNecessary(token_a, token_b, fee, x96_price, {"from": accounts[0]})
 
+    # mon petit contrat d'utilitaires uniswap
+    uni_utils = UniUtils.deploy({"from": accounts[0]})
+    
+    # add liq
+
+    # params
+    min_price = 1
+    max_price = 2
+
+    #block_number = web3.eth.block_number
+    #block_timestamp = web3.eth.get_block(block_number).timestamp
+
+
+    '''
+    struct MintParams {
+        address token0;
+        address token1;
+        uint24 fee;
+        int24 tickLower;
+        int24 tickUpper;
+        uint256 amount0Desired;
+        uint256 amount1Desired;
+        uint256 amount0Min;
+        uint256 amount1Min;
+        address recipient;
+        uint256 deadline;
+    }
+    '''
+    
+    token_a.approve(pos_manager, 2**256 - 1, {'from':accounts[1]})
+    token_b.approve(pos_manager, 2**256 - 1, {'from':accounts[1]})
+
+    my_pos = pos_manager.mint((token_b, 
+    token_a, 
+    fee,
+    -60, # uni_utils.ratioToTick(sqrtRatioX96_ofPrice(min_price)),
+    60, # uni_utils.ratioToTick(sqrtRatioX96_ofPrice(max_price)),
+    1*1e18,
+    1*1e18,
+    0,
+    0,
+    accounts[1],
+    10000000000000), {"from": accounts[1]})
+
+
+    # swap(s)
+    raise exception
