a6aa0299001e56c0e369c93564efb45689571f1a
Diego Legrand <2536573+DiegoRider@users.noreply.github.com>
2022-07-20 12:21:54 +0200

    fix tick spacing

--- scripts/uniswap_deployer.py
+++ scripts/uniswap_deployer.py
@@ -7,6 +7,37 @@ from eth_typing import BlockNumber
 from pathlib import Path
 import math
 
+FEE_LIST = [500, 3000, 10000]
+TICK_SPACING_LIST = [10, 60, 200]
+
+def print_events(tx):
+    for key in tx.events.keys():
+        print(key)
+        for item in tx.events[key]:
+            print(item)
+
+def getTick(uni_utils, price, spacing, floor=True): 
+    tick = uni_utils.ratioToTick(sqrtRatioX96_ofPrice(price))
+    if floor == True:
+        tick = tick - tick % spacing
+    else:
+        tick = tick + (-tick) % spacing
+    return tick
+
+def sqrtRatioX96_ofPrice(price):
+    return math.sqrt(price) * 2 ** 96
+
+'''
+sqrtPriceX96 = sqrt(price) * 2 ** 96
+# divide both sides by 2 ** 96
+sqrtPriceX96 / (2 ** 96) = sqrt(price)
+# square both sides
+(sqrtPriceX96 / (2 ** 96)) ** 2 = price
+# expand the squared fraction
+(sqrtPriceX96 ** 2) / ((2 ** 96) ** 2)  = price
+# multiply the exponents in the denominator to get the final expression
+sqrtRatioX96 ** 2 / 2 ** 192 = price
+'''
 
 
 
@@ -20,82 +51,40 @@ def main():
         token_a.mint(accounts[i], 10*1e18)
         token_b.mint(accounts[i], 10*1e18)
 
-    # -------------------------------------------------------
-    # on créer une pool uniswap-v3
-    # -------------------------------------------------------
-    print("depoying uniswap factory")
+    print("deploying uniswap utility contract")
+    uni_utils = UniUtils.deploy({"from": accounts[0]})
+
+    print("deploying uniswap factory")
     uniswap_core = project.load("Uniswap/v3-core@1.0.0")
     uni_factory = uniswap_core.UniswapV3Factory.deploy({"from": accounts[0]})
-
-    #address _WETH9
     weth9 = accounts[0]
-
-    #address _tokenDescriptor_
     tokenDescriptor = accounts[0]
-    '''
-    sqrtPriceX96 = sqrt(price) * 2 ** 96
-    # divide both sides by 2 ** 96
-    sqrtPriceX96 / (2 ** 96) = sqrt(price)
-    # square both sides
-    (sqrtPriceX96 / (2 ** 96)) ** 2 = price
-    # expand the squared fraction
-    (sqrtPriceX96 ** 2) / ((2 ** 96) ** 2)  = price
-    # multiply the exponents in the denominator to get the final expression
-    sqrtRatioX96 ** 2 / 2 ** 192 = price
-    '''
-
-    # params 
-    fee_list = [500, 3000, 10000]
     
-    # vars
-    price = 1.2654
-    fee = fee_list[1]
-
-    # fonctions
-    def sqrtRatioX96_ofPrice(price):
-        return math.sqrt(price) * 2 ** 96
-    x96_price = sqrtRatioX96_ofPrice(price)
-
-    # NonFungiblePositionManager
     print("depoying NonfungiblePositionManager")
     nft_pos_manager = project.load("Uniswap/v3-periphery@1.0.0").NonfungiblePositionManager
     pos_manager = nft_pos_manager.deploy(uni_factory, weth9, tokenDescriptor, {"from": accounts[0]})
 
+    # pool parameters
+    price = 1.2654
+    fee = FEE_LIST[1]
+    tick_spacing = TICK_SPACING_LIST[1]
 
-    # initialize pool
     print("initializing pool")
     my_pool_add = pos_manager.createAndInitializePoolIfNecessary(
         token_a, 
         token_b, 
         fee, 
-        x96_price, 
+        sqrtRatioX96_ofPrice(price), 
         {"from": accounts[0]}
     )
     print("pool created events: ")
-    print(my_pool_add.events)
+    print_events(my_pool_add)
 
-    # mon petit contrat d'utilitaires uniswap
-    uni_utils = UniUtils.deploy({"from": accounts[0]})
-
+    print("adding liquidity")
     # # params
     min_price = 1
     max_price = 2
-
-    '''
-    struct MintParams {
-        address token0;
-        address token1;
-        uint24 fee;
-        int24 tickLower;
-        int24 tickUpper;
-        uint256 amount0Desired;
-        uint256 amount1Desired;
-        uint256 amount0Min;
-        uint256 amount1Min;
-        address recipient;
-        uint256 deadline;
-    }
-    '''
+    
     # #block_number = web3.eth.block_number
     # #block_timestamp = web3.eth.get_block(block_number).timestamp
     
@@ -107,26 +96,23 @@ def main():
     # POOL_INIT_CODE_HASH = keccak256(abi.encodePacked(type(UniswapV3Pool).creationCode)); in v3-core/contracts/UniswapV3PoolDeployer.sol
     my_pos = pos_manager.mint(
         (
-            token_a, 
-            token_b, 
-            fee,
-            -60, #uni_utils.ratioToTick(sqrtRatioX96_ofPrice(min_price)),
-            60, #uni_utils.ratioToTick(sqrtRatioX96_ofPrice(max_price)),
-            1*1e18,
-            1*1e18,
-            0,
-            0,
-            accounts[1].address,
-            10000000000000
+            token_a, # token0
+            token_b, # token1
+            fee, # fee
+            getTick(uni_utils, min_price, tick_spacing, floor=True), # tickLower
+            getTick(uni_utils, max_price, tick_spacing, floor=False), # tickLower
+            1*1e18, # amount0Desired
+            1*1e18, # amount1Desired
+            0, # amount0Min
+            0, # amount1Min
+            accounts[1].address, # recipient
+            10000000000000 # deadline
         ),
         {
             "from": accounts[1]
         }
     )
-
-    print('add liquidity events')
-    print(my_pos.events)
-
+    print_events(my_pos)
 
     # swap(s)
     raise Exception
